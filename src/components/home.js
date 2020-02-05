db7e045687a86810c70a668a1b4e7851796f6689
Michael <michaelli.sw.eng@gmail.com>
2020-02-05 04:25:53 -0800

    Added home page UI

--- src/components/home.js
+++ src/components/home.js
@@ -1,11 +1,106 @@
 import React, { Component } from "react";
 import '../assets/css/home.css';
+import { MdPlayCircleOutline } from "react-icons/md";
 
 class Home extends Component {
   	render() {
 	    return(
 			<div id="home">
-			
+				<div id="title">Discover Weekly</div>
+				<div id="weeklySongsContainer">
+					<div className="songContainer">
+						<div className="songImg"></div>
+						<div className="playSongIconContainer"><MdPlayCircleOutline className="playSongIcon" /></div>
+						<div className="songName">Hello</div>
+						<div className="songArtist">Adelle</div>
+					</div>
+					<div className="songContainer">
+						<div className="songImg"></div>
+						<div className="playSongIconContainer"><MdPlayCircleOutline className="playSongIcon" /></div>
+						<div className="songName">Hello</div>
+						<div className="songArtist">Adelle</div>
+					</div>
+					<div className="songContainer">
+						<div className="songImg"></div>
+						<div className="playSongIconContainer"><MdPlayCircleOutline className="playSongIcon" /></div>
+						<div className="songName">Hello</div>
+						<div className="songArtist">Adelle</div>
+					</div>
+					<div className="songContainer">
+						<div className="songImg"></div>
+						<div className="playSongIconContainer"><MdPlayCircleOutline className="playSongIcon" /></div>
+						<div className="songName">Hello</div>
+						<div className="songArtist">Adelle</div>
+					</div>
+					<div className="songContainer">
+						<div className="songImg"></div>
+						<div className="playSongIconContainer"><MdPlayCircleOutline className="playSongIcon" /></div>
+						<div className="songName">Hello</div>
+						<div className="songArtist">Adelle</div>
+					</div>
+
+					<div className="songContainer">
+						<div className="songImg"></div>
+						<div className="playSongIconContainer"><MdPlayCircleOutline className="playSongIcon" /></div>
+						<div className="songName">Hello</div>
+						<div className="songArtist">Adelle</div>
+					</div>
+					<div className="songContainer">
+						<div className="songImg"></div>
+						<div className="playSongIconContainer"><MdPlayCircleOutline className="playSongIcon" /></div>
+						<div className="songName">Hello</div>
+						<div className="songArtist">Adelle</div>
+					</div>
+					<div className="songContainer">
+						<div className="songImg"></div>
+						<div className="playSongIconContainer"><MdPlayCircleOutline className="playSongIcon" /></div>
+						<div className="songName">Hello</div>
+						<div className="songArtist">Adelle</div>
+					</div>
+					<div className="songContainer">
+						<div className="songImg"></div>
+						<div className="playSongIconContainer"><MdPlayCircleOutline className="playSongIcon" /></div>
+						<div className="songName">Hello</div>
+						<div className="songArtist">Adelle</div>
+					</div>
+					<div className="songContainer">
+						<div className="songImg"></div>
+						<div className="playSongIconContainer"><MdPlayCircleOutline className="playSongIcon" /></div>
+						<div className="songName">Hello</div>
+						<div className="songArtist">Adelle</div>
+					</div>
+
+					<div className="songContainer">
+						<div className="songImg"></div>
+						<div className="playSongIconContainer"><MdPlayCircleOutline className="playSongIcon" /></div>
+						<div className="songName">Hello</div>
+						<div className="songArtist">Adelle</div>
+					</div>
+					<div className="songContainer">
+						<div className="songImg"></div>
+						<div className="playSongIconContainer"><MdPlayCircleOutline className="playSongIcon" /></div>
+						<div className="songName">Hello</div>
+						<div className="songArtist">Adelle</div>
+					</div>
+					<div className="songContainer">
+						<div className="songImg"></div>
+						<div className="playSongIconContainer"><MdPlayCircleOutline className="playSongIcon" /></div>
+						<div className="songName">Hello</div>
+						<div className="songArtist">Adelle</div>
+					</div>
+					<div className="songContainer">
+						<div className="songImg"></div>
+						<div className="playSongIconContainer"><MdPlayCircleOutline className="playSongIcon" /></div>
+						<div className="songName">Hello</div>
+						<div className="songArtist">Adelle</div>
+					</div>
+					<div className="songContainer">
+						<div className="songImg"></div>
+						<div className="playSongIconContainer"><MdPlayCircleOutline className="playSongIcon" /></div>
+						<div className="songName">Hello</div>
+						<div className="songArtist">Adelle</div>
+					</div>
+				</div>
 			</div>
 	    );
   	}
